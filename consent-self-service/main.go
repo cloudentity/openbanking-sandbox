6cb9d72562672cc151a44a477a373e8c78e4bff8
Ireneusz Kawalec <ikawalec@cloudentity.com>
2020-11-27 12:26:44 +0100

    consent self service app init

---
+++ consent-self-service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/caarlos0/env/v6"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+type Config struct {
+}
+
+func LoadConfig() (config Config, err error) {
+	if err = env.Parse(&config); err != nil {
+		return config, err
+	}
+
+	return config, err
+}
+
+type Server struct {
+	Config Config
+}
+
+func NewServer() (Server, error) {
+	var (
+		server = Server{}
+		err    error
+	)
+
+	if server.Config, err = LoadConfig(); err != nil {
+		return server, errors.Wrapf(err, "failed to load config")
+	}
+
+	return server, nil
+}
+
+func (s *Server) Start() error {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
+
+	r.GET("/", s.Get())
+
+	return r.Run()
+}
+
+func main() {
+	var (
+		server Server
+		err    error
+	)
+
+	if server, err = NewServer(); err != nil {
+		logrus.WithError(err).Fatalf("failed to init server")
+	}
+
+	if err = server.Start(); err != nil {
+		logrus.WithError(err).Fatalf("failed to start server")
+	}
+}
