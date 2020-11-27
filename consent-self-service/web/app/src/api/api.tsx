87abb826b36c8229f6648e976401c6b718273d7a
Bartosz Wereszczak <bwereszczak@syntegrity.com>
2020-11-27 20:51:56 +0100

    Get and revoke consents

--- consent-self-service/web/app/src/api/api.tsx
+++ consent-self-service/web/app/src/api/api.tsx
@@ -1,5 +1,7 @@
-import {baseWithCustomBaseUrl} from './api-base';
+import {base, baseWithCustomBaseUrl} from './api-base';
 
 export const api = {
-  userinfo: (authorizationServerURL, tenantId, authorizationServerId) => baseWithCustomBaseUrl('/', authorizationServerURL).get({url: `/${tenantId}/${authorizationServerId}/userinfo`}),
+    userinfo: (authorizationServerURL, tenantId, authorizationServerId) => baseWithCustomBaseUrl('/', authorizationServerURL).get({url: `/${tenantId}/${authorizationServerId}/userinfo`}),
+    getConsents: () => base.get({url: `/consents`}),
+    deleteConsent: ({id}) => base.delete({url: `/consents/${id}`, query: {}}),
 };
--- consent-self-service/web/app/src/components/Dashboard.tsx
+++ consent-self-service/web/app/src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PageToolbar from "./PageToolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,7 +9,8 @@ import Tabs from "@material-ui/core/Tabs";
 import {
     Accordion,
     AccordionDetails,
-    AccordionSummary, Avatar,
+    AccordionSummary,
+    Avatar,
     Button,
     Container,
     Grid,
@@ -19,6 +20,7 @@ import {
 import {logout} from "./AuthPage";
 import {makeStyles} from "@material-ui/core/styles";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {api} from "../api/api";
 
 const useStyles = makeStyles((theme: Theme) => ({
     indicator: {
@@ -26,99 +28,30 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
 }));
 
-const consents = [
-    {
-        name: "Financroo",
-        id: "1234567",
-        granted: "1 jan 2001",
-        valid: "2 jan 2002",
-        accounts: [
-            {
-                accountId: "233q432423",
-                permissions: [
-                    {
-                        name: "permissionNamePermissionNamePermissionnanamam",
-                        description: "descdescdescdescdescdesc descdesc descdescdescdescdesc descdesc descdescdesc descdesc descdesc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    }
-                ]
-            },
-            {
-                accountId: "233q43223",
-                permissions: [
-                    {
-                        name: "permission 2",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 2",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        name: "Financroo2",
-        id: "1234567",
-        granted: "1 jan 2001",
-        valid: "2 jan 2002",
-        accounts: [
-            {
-                accountId: "233q432423",
-                permissions: [
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    }
-                ]
-            },
-            {
-                accountId: "233q43223",
-                permissions: [
-                    {
-                        name: "permission 2",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 2",
-                        description: "desc"
-                    },
-                    {
-                        name: "permission 1",
-                        description: "desc"
-                    }
-                ]
-            }
-        ]
-    }
-]
-
 export default function Dashboard({authorizationServerURL, authorizationServerId, tenantId}) {
-    const isProgress = false;
+    const [isProgress, setProgress] = useState(true);
+    const [consents, setConsents] = useState<any>([]);
     const classes = useStyles();
 
+    useEffect(() => {
+        setProgress(true);
+        api.getConsents()
+            .then(res => setConsents(res))
+            .catch(err => console.log(err))
+            .finally(() => setProgress(false));
+    }, []);
+
+    const handleRemove = id => {
+        setProgress(true);
+        api.deleteConsent({id})
+            .then(api.getConsents)
+            .then(res => setConsents(res))
+            .catch(err => console.log(err))
+            .finally(() => setProgress(false));
+    }
+
     return (
-        <div style={{position: 'relative'}}>
+        <div>
             <PageToolbar>
                 <Hidden mdUp>
                     <IconButton edge="start" color="inherit" aria-label="menu">
@@ -136,11 +69,16 @@ export default function Dashboard({authorizationServerURL, authorizationServerId
                         aria-label="menu tabs"
                         style={{height: 64}}
                     >
-                        <Tab label="Get started" value={'get-started'} style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
-                        <Tab label="APIs" value={'apis'} style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
-                        <Tab label="Help Center" value={'help-center'} style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
-                        <Tab label="Connected Applications" value={'connected-applications'} style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
-                        <Tab label="Monitoring" value={'monitoring'} style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
+                        <Tab label="Get started" value={'get-started'}
+                             style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
+                        <Tab label="APIs" value={'apis'}
+                             style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
+                        <Tab label="Help Center" value={'help-center'}
+                             style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
+                        <Tab label="Connected Applications" value={'connected-applications'}
+                             style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
+                        <Tab label="Monitoring" value={'monitoring'}
+                             style={{height: 64, textTransform: "none", minWidth: "unset"}}/>
                     </Tabs>
                 </Hidden>
                 <div style={{flex: 1}}/>
@@ -148,81 +86,97 @@ export default function Dashboard({authorizationServerURL, authorizationServerId
                     style={{color: '#fff'}}
                     onClick={() => logout(authorizationServerURL, tenantId, authorizationServerId)}>Logout</Button>
             </PageToolbar>
-            {isProgress && <Progress/>}
+            <div style={{marginTop: 64, position: "relative"}}>
+                {isProgress && <Progress/>}
 
-            {!isProgress && (
-                <>
-                    <div style={{
-                        background: "#F7F7F7",
-                        height: 148,
-                        marginTop: 64,
-                        display: 'flex',
-                        alignItems: 'center'
-                    }}>
-                        <Container>
-                            <Typography variant={"h3"} color={"primary"}>Connected Applications</Typography>
-                        </Container>
-                    </div>
-                    <Container style={{marginTop: 64}}>
-                        <Grid container justify={"center"}>
-                            <Grid item xs={8}>
-                                {consents.map(consent => (
-                                    <Accordion>
-                                        <AccordionSummary
-                                            expandIcon={<ExpandMoreIcon/>}
-                                            aria-controls="panel1a-content"
-                                            id="panel1a-header"
-                                        >
-                                            <div style={{
-                                                display: "flex",
-                                                justifyContent: "space-between",
-                                                alignItems: "center",
-                                                width: '100%'
-                                            }}>
-                                                <div style={{display: "flex", alignItems: "center"}}>
-                                                    <Avatar>{consent.name[0]}</Avatar>
-                                                    <Typography style={{marginLeft: 16}}>{consent.name}</Typography>
+                {!isProgress && (
+                    <>
+                        <div style={{
+                            background: "#F7F7F7",
+                            height: 148,
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}>
+                            <Container>
+                                <Typography variant={"h3"} color={"primary"}>Connected Applications</Typography>
+                            </Container>
+                        </div>
+                        <Container style={{marginTop: 64}}>
+                            <Grid container justify={"center"}>
+                                <Grid item xs={8}>
+                                    {consents.length === 0 && (
+                                        <Typography>No connected applications</Typography>
+                                    )}
+                                    {consents.map(consent => (
+                                        <Accordion>
+                                            <AccordionSummary
+                                                expandIcon={<ExpandMoreIcon/>}
+                                                aria-controls="panel1a-content"
+                                                id="panel1a-header"
+                                            >
+                                                <div style={{
+                                                    display: "flex",
+                                                    justifyContent: "space-between",
+                                                    alignItems: "center",
+                                                    width: '100%'
+                                                }}>
+                                                    <div style={{display: "flex", alignItems: "center"}}>
+                                                        <Avatar>{consent.name[0]}</Avatar>
+                                                        <Typography style={{marginLeft: 16}}>{consent.name}</Typography>
+                                                    </div>
+                                                    <div style={{flex: 1}}/>
+                                                    <Button variant={"contained"} color={"primary"} onClick={e => {
+                                                        e.stopPropagation();
+                                                        handleRemove(consent.id);
+                                                    }}>Revoke</Button>
                                                 </div>
-                                                <div style={{flex: 1}}/>
-                                                <Button variant={"contained"} color={"primary"} onClick={e => {
-                                                    e.stopPropagation()
-                                                }}>Revoke</Button>
-                                            </div>
-                                        </AccordionSummary>
-                                        <AccordionDetails style={{flexDirection: "column", background: "#fcfcfc"}}>
-                                            {consent.accounts.map(account => (
-                                                <div style={{marginBottom: 32}}>
-                                                    <Typography>
-                                                        <strong>Account ID: {account.accountId}</strong>
-                                                    </Typography>
-                                                    <Typography style={{marginTop: 16, borderBottom: "1px solid #ccc", paddingBottom: 8, marginBottom: 16}}>Granted permissions:</Typography>
-                                                    <Grid container spacing={2}>
-                                                        {account.permissions.map(permission => (
-                                                            <>
-                                                                <Grid item xs={4}
-                                                                      wrap={"wrap"}
-                                                                      style={{textAlign: "right", wordWrap: "break-word"}}>
-                                                                    <Typography><strong>{permission.name}</strong></Typography>
-                                                                </Grid>
-                                                                <Grid item xs={8}
-                                                                      style={{textAlign: "left", wordWrap: "break-word"}}>
-                                                                    <Typography>{permission.description}</Typography>
-                                                                </Grid>
-                                                            </>
-                                                        ))}
+                                            </AccordionSummary>
+                                            <AccordionDetails style={{flexDirection: "column", background: "#fcfcfc"}}>
+                                                {consent.accounts.map(account => (
+                                                    <div style={{marginBottom: 32}}>
+                                                        <Typography>
+                                                            <strong>Account ID: {account.accountId}</strong>
+                                                        </Typography>
+                                                        <Typography style={{
+                                                            marginTop: 16,
+                                                            borderBottom: "1px solid #ccc",
+                                                            paddingBottom: 8,
+                                                            marginBottom: 16
+                                                        }}>Granted permissions:</Typography>
+                                                        <Grid container spacing={2}>
+                                                            {account.permissions.map(permission => (
+                                                                <>
+                                                                    <Grid item xs={4}
+                                                                          wrap={"wrap"}
+                                                                          style={{
+                                                                              textAlign: "right",
+                                                                              wordWrap: "break-word"
+                                                                          }}>
+                                                                        <Typography><strong>{permission.name}</strong></Typography>
+                                                                    </Grid>
+                                                                    <Grid item xs={8}
+                                                                          style={{
+                                                                              textAlign: "left",
+                                                                              wordWrap: "break-word"
+                                                                          }}>
+                                                                        <Typography>{permission.description}</Typography>
+                                                                    </Grid>
+                                                                </>
+                                                            ))}
 
-                                                    </Grid>
-                                                </div>
-                                            ))}
-                                        </AccordionDetails>
-                                    </Accordion>
-                                ))}
+                                                        </Grid>
+                                                    </div>
+                                                ))}
+                                            </AccordionDetails>
+                                        </Accordion>
+                                    ))}
+                                </Grid>
                             </Grid>
-                        </Grid>
 
-                    </Container>
-                </>
-            )}
+                        </Container>
+                    </>
+                )}
+            </div>
         </div>
     )
 };
