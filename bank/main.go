cc49216a2ae4d55ba01774afb3fba5cdc94b90f5
Ireneusz Kawalec <ikawalec@cloudentity.com>
2020-11-27 11:49:27 +0100

    fixes

--- bank/main.go
+++ bank/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/caarlos0/env/v6"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 	logrus "github.com/sirupsen/logrus"
 )
 
@@ -68,7 +67,7 @@ func main() {
 		err    error
 	)
 
-	log.SetFormatter(&log.JSONFormatter{})
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	if server, err = NewServer(); err != nil {
 		logrus.WithError(err).Fatalf("failed to init server")
