deea01eac35eab7d00505a762bbe45d23ab99bc6
Ireneusz Kawalec <ikawalec@cloudentity.com>
2020-11-30 11:00:46 +0100

    add authorization for revoke

--- consent-self-service/handler.go
+++ consent-self-service/handler.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 
 	"github.com/cloudentity/acp/pkg/swagger/models"
 )
@@ -19,43 +18,69 @@ func (s *Server) Index() func(*gin.Context) {
 func (s *Server) ListConsents() func(*gin.Context) {
 	return func(c *gin.Context) {
 		var (
-			accounts           InternalAccounts
 			consentsByAccounts *models.ListConsentsByAccountsResponse
 			err                error
 		)
 
-		subject := "subject" // todo
-
-		if accounts, err = s.BankClient.GetInternalAccounts(subject); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("failed to get accounts from bank: %+v", err))
+		if consentsByAccounts, err = s.FetchAccounts(c); err != nil {
+			c.String(http.StatusBadRequest, err.Error())
 			return
 		}
-		logrus.Infof("accounts: %+v", accounts)
-
-		accountIDs := make([]string, len(accounts.Accounts))
-		for i, a := range accounts.Accounts {
-			accountIDs[i] = a.ID
-		}
-
-		if consentsByAccounts, err = s.Client.GetAccountAccessConsent(accountIDs); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("failed to get account access consent from acp: %+v", err))
-			return
-		}
-		logrus.Infof("consentsByAccounts: %+v", consentsByAccounts)
 
 		c.JSON(http.StatusOK, &consentsByAccounts)
 	}
 }
 
+func (s *Server) FetchAccounts(c *gin.Context) (*models.ListConsentsByAccountsResponse, error) {
+	var (
+		accounts           InternalAccounts
+		consentsByAccounts *models.ListConsentsByAccountsResponse
+		err                error
+	)
+
+	subject := "subject" // todo introspect token
+
+	if accounts, err = s.BankClient.GetInternalAccounts(subject); err != nil {
+		return nil, fmt.Errorf("failed to get accounts from bank: %w", err)
+	}
+
+	accountIDs := make([]string, len(accounts.Accounts))
+	for i, a := range accounts.Accounts {
+		accountIDs[i] = a.ID
+	}
+
+	if consentsByAccounts, err = s.Client.GetAccountAccessConsent(accountIDs); err != nil {
+		return nil, fmt.Errorf("failed to get account access consent from acp: %wv", err)
+	}
+
+	return consentsByAccounts, nil
+}
+
 func (s *Server) RevokeConsent() func(*gin.Context) {
 	return func(c *gin.Context) {
 		var (
-			id  = c.Param("id")
-			err error
+			id                 = c.Param("id")
+			consentsByAccounts *models.ListConsentsByAccountsResponse
+			canBeRevoked       bool
+			err                error
 		)
-		logrus.Infof("revoke consent: %s", id)
 
-		// todo authz
+		if consentsByAccounts, err = s.FetchAccounts(c); err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		for _, c := range consentsByAccounts.Consents {
+			if c.ConsentID == id {
+				canBeRevoked = true
+				break
+			}
+		}
+
+		if !canBeRevoked {
+			c.String(http.StatusBadRequest, "user is not authorized to revoke this consent")
+		}
+
 		if err = s.Client.RevokeAccountAccessConsent(id); err != nil {
 			c.String(http.StatusBadRequest, fmt.Sprintf("failed to revoke account access consent: %+v", err))
 			return
