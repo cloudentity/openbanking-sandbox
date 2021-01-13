a0dc5084490fa8d4cb56fb5d06195f059fe053e7
Marcin Żerko <69037127+mzerko-cloudentity@users.noreply.github.com>
2021-01-13 15:24:33 +0100

    Fixed selector

--- consent-admin/web/app/src/components/ClientCard.tsx
+++ consent-admin/web/app/src/components/ClientCard.tsx
@@ -61,7 +61,7 @@ export default function ClientCard({client, onRevokeClient, onRevokeConsent}) {
                             <div style={{marginLeft: 24, display: 'flex', justifyContent: 'space-between'}}>
                                 <Typography>Revoke all the client consents which access the Financial
                                     information</Typography>
-                                <Button class={"revoke-all-button"} variant={'contained'}
+                                <Button className={"revoke-all-button"} variant={'contained'}
                                         style={{background: '#DC1B37', color: '#fff'}}
                                         onClick={onRevokeClient(client.client_id)}>Revoke all</Button>
                             </div>
