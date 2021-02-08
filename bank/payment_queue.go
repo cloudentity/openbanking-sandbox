397d46351bff499ebce344e2181af6794a6835c6
Dylan Martin <dmartin@cloudentity.com>
2021-02-08 10:55:34 -0500

    adds simple payment queue implementation to update domestic payment status

---
+++ bank/payment_queue.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	paymentModels "github.com/cloudentity/openbanking-sandbox/openbanking/paymentinitiation/models"
+)
+
+type PaymentQueue struct {
+	repo  UserRepo
+	queue chan (paymentModels.OBWriteDomesticResponse5)
+}
+
+func NewPaymentQueue(repo UserRepo) (PaymentQueue, error) {
+	return PaymentQueue{
+		repo:  repo,
+		queue: make(chan paymentModels.OBWriteDomesticResponse5),
+	}, nil
+}
+
+func (pq *PaymentQueue) Start() {
+	for {
+		select {
+		case payment := <-pq.queue:
+			time.Sleep(10 * time.Second)
+			pq.repo.SetDomesticPaymentStatus(*payment.Data.DomesticPaymentID, AcceptedCreditSettlementCompleted)
+		}
+	}
+}
