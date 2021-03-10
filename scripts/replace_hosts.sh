5d66d10f0c47e4d1b7c8a9456b2a5ea41808c63c
Ireneusz Kawalec <ikawalec@cloudentity.com>
2021-03-10 17:26:25 +0100

    configurable .env file

--- scripts/replace_hosts.sh
+++ scripts/replace_hosts.sh
@@ -3,7 +3,9 @@
 SEED="data/seed.yaml"
 SEED_GENERATED="data/seed.yaml.generated"
 
-export $(cat .env | sed 's/#.*//g' | xargs)
+env_file=${ENV:-.env}
+
+export $(cat ${env_file} | sed 's/#.*//g' | xargs)
 
 cp $SEED $SEED_GENERATED
 sed -i.bak "s|https:\/\/localhost:8443|${ACP_URL}|g" ${SEED_GENERATED} && rm "${SEED_GENERATED}.bak"
